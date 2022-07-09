fdba99638546290c8a3924fd476702ca8192ef3c
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-07-09 15:45:56 +0200

    Sol_Step_02

--- Tp1.py
+++ Tp1.py
@@ -1,4 +1,9 @@
+import math
+
+
 class Tp1:
+    # region Bonus
+
     """
     This method is designed to gets the sum of 2 numbers
     @:param number1:
@@ -20,15 +25,6 @@ class Tp1:
         return number1 - number2
 
     """
-    This method is designed to gets the triple of a number
-    @:param number: integer to triple
-    @:return : number * 3
-    """
-
-    def triple(self, number):
-        return number * 3
-
-    """
     This method is designed to gets the average a 3 numbers
     @:param number1 : 
     @:param number2 : 
@@ -38,6 +34,17 @@ class Tp1:
 
     def average(self, number1, number2, number3):
         return (number1 + number2 + number3) / 3
+
+    """
+    This method is designed to gets the triple of a number
+    @:param number: integer to triple
+    @:return : number * 3
+    """
+
+    # endregion Bonus
+
+    def triple(self, number):
+        return number * 3
 
     """
     This method is designed to test if a number is peer
@@ -50,3 +57,95 @@ class Tp1:
             return True
         else:
             return False
+
+    """
+    This method is designed to realize multiplications
+    @:param multiplier :
+    @:param multiplicand:
+    @:return : the multiplication's result
+    """
+
+    def multiply(self, multiplier, multiplicand):
+        return multiplier * multiplicand
+
+    """
+    This method is designed to generate a list of peer number
+    @:param range_upper_limit :
+    @:return a list of peer numbers (from 0 to range_upper_limit)
+    https://www.w3schools.com/python/python_lists.asp
+    """
+
+    def unpair_range(self, range_upper_limit):
+        list_of_peers = []
+        for number in range(range_upper_limit):
+            if not self.is_peer(number):
+                list_of_peers.append(number)
+        return list_of_peers
+
+    """
+    This method is designed to generate a list of pair, triplied
+    @:param range_upper_limit :
+    @:return a list of peer numbers triplied (from 0 to range_upper_limit)
+    https://www.w3schools.com/python/python_lists.asp
+    """
+
+    def triple_range(self, range_upper_limit):
+        list_of_triples = []
+        for number in range(1, range_upper_limit + 1):
+            list_of_triples.append(self.triple(number))
+        return list_of_triples
+
+    """
+    This method is designed to get the factorial of a number
+    @:param number :
+    @:return the factorial of a number
+    """
+
+    def factorial(self, number):
+        return math.factorial(number)
+
+    """
+    This method is designed to convert a list of number in a list of number and boolean (true, false if peer)
+    @:param list of numbers : sample (1, 4, 3, 7)
+    @:return the list : sample ((1, False), (4, True), (3, False), (7, False))
+    https://www.programiz.com/python-programming/tuple
+    """
+
+    def integer_to_boolean(self, numbers):
+        list_numbers_to_boolean = []
+        for number in numbers:
+            if self.is_peer(number):
+                list_numbers_to_boolean.append((number, True))
+            else:
+                list_numbers_to_boolean.append((number, False))
+        return list_numbers_to_boolean
+
+    """
+    This method is designed to get the population's average by city
+    @:param cities : sample ('Zürich', 409241), ('Genève', 200548), ('Basel', 171513), ('Bern', 133798)
+    @:return population average by city : sample 210801
+    https://www.programiz.com/python-programming/tuple
+    """
+
+    def population_average_by_city(self, cities):
+        population_total = 0
+        for city in cities:
+            population_total += city[1]
+        amount_city = len(cities)
+        return population_total / amount_city
+
+    """
+    This method is designed to find a first bigger value from a minimal in a given list
+    @:param numbers : sample [7, 1, 4, 0]
+    @:param min_value : sample 3
+    @:return either the value found (sample : 4), or "None"
+    https://www.programiz.com/python-programming/tuple
+    """
+
+    def find_value_bigger_than_minimal(self, numbers, min_value):
+
+        numbers.sort()
+        for number in numbers:
+            if number > min_value:
+                return number
+        return None
--- tests/testTp1.py
+++ tests/testTp1.py
@@ -12,6 +12,8 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.tp1 = Tp1()
 
+    # region Bonus
+    # Bonus
     def test_add_nominal_case_success(self):
         # given
         # refer to setup method
@@ -25,6 +27,7 @@ class TestModel(unittest.TestCase):
         # then
         self.assertEqual(expected_result, actual_result)
 
+    # Bonus
     def test_subtract_nominal_case_success(self):
         # given
         # refer to setup method
@@ -38,18 +41,7 @@ class TestModel(unittest.TestCase):
         # then
         self.assertEqual(expected_result, actual_result)
 
-    def test_triple_nominal_case_success(self):
-        # given
-        # refer to setup method
-        number = 5
-        expected_result = 15
-
-        # when
-        actual_result = self.tp1.triple(number)
-
-        # then
-        self.assertEqual(expected_result, actual_result)
-
+    # Bonus
     def test_average_nominal_case_success(self):
         # given
         # refer to setup method
@@ -64,6 +56,22 @@ class TestModel(unittest.TestCase):
         # then
         self.assertEqual(expected_result, actual_result)
 
+    # endregion Bonus
+
+    # Question 1
+    def test_triple_nominal_case_success(self):
+        # given
+        # refer to setup method
+        number = 5
+        expected_result = 15
+
+        # when
+        actual_result = self.tp1.triple(number)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
+
+    # Question 2
     def test_is_peer_true_success(self):
         # given
         # refer to setup method
@@ -75,6 +83,7 @@ class TestModel(unittest.TestCase):
         # then
         self.assertTrue(self.tp1.is_peer(number))
 
+    # Question 2
     def test_is_peer_false_success(self):
         # given
         # refer to setup method
@@ -85,3 +94,108 @@ class TestModel(unittest.TestCase):
 
         # then
         self.assertFalse(self.tp1.is_peer(number))
+
+    # Question 3
+    def test_multiply_nominal_case_success(self):
+        # given
+        multiplicand = 5
+        multiplier = 3
+        expected_result = 15
+
+        # when
+        actual_result = self.tp1.multiply(multiplicand, multiplier)
+
+        # then
+        self.assertEqual(actual_result, expected_result)
+
+    # Question 4
+    def test_unpair_range_nominal_case_success(self):
+        # given
+        range_upper_limit = 10
+        expected_result = list((1, 3, 5, 7, 9))
+
+        # when
+        actual_result = self.tp1.unpair_range(range_upper_limit)
+
+        # then
+        self.assertCountEqual(expected_result, actual_result)
+
+    # Question 5
+    def test_triple_range_nominal_case_success(self):
+        # given
+        range_upper_limit = 10
+        expected_result = list((3, 6, 9, 12, 15, 18, 21, 24, 27, 30))
+
+        # when
+        actual_result = self.tp1.triple_range(range_upper_limit)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
+
+    # Question 6
+    # TODO
+
+    # Question 7
+    # TODO
+
+    # Question 8
+    def test_factorial_nominal_case_success(self):
+        # given
+        number = 4
+        expected_result = 24
+
+        # when
+        actual_result = self.tp1.factorial(number)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
+
+    # Question 9
+    def test_integer_to_boolean_nominal_case_success(self):
+        # given
+        numbers = list((1, 4, 3, 7))
+        expected_result = list(((1, False), (4, True), (3, False), (7, False)))
+
+        # when
+        actual_result = self.tp1.integer_to_boolean(numbers)
+
+        # then
+        self.assertCountEqual(expected_result, actual_result)
+
+    # Question 10
+    def test_population_average_by_city_nominal_case_success(self):
+        # given
+        cities = list((('Zürich', 409241), ('Genève', 200548), ('Basel', 171513), ('Bern', 133798)))
+        expected_result = 228775
+
+        # when
+        actual_result = self.tp1.population_average_by_city(cities)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
+
+    # Question 11 a
+    def test_find_value_bigger_than_minimal_nominal_case_success(self):
+        # given
+        numbers = list((7, 1, 4, 0))
+        min_value = 3
+        expected_result = 4
+
+        # when
+        actual_result = self.tp1.find_value_bigger_than_minimal(numbers, min_value)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
+
+    # Question 11 b
+    def test_find_value_bigger_than_minimal_none_success(self):
+        # given
+        numbers = list((7, 1, 4, 0))
+        min_value = 8
+        expected_result = None
+
+        # when
+        actual_result = self.tp1.find_value_bigger_than_minimal(numbers, min_value)
+
+        # then
+        self.assertEqual(expected_result, actual_result)
